bbce48a49abcb50148ce8ccfabebec5c1677b9a1
Daniel G <danielifguerrero@gmail.com>
2024-04-01 14:41:31 -0500

    debug

--- src/service/util/loadKeys.ts
+++ src/service/util/loadKeys.ts
@@ -5,25 +5,27 @@ import { logErrorToFile } from "../../../error/logger";
 
 
 export async function _loadEnvVars(ctx: any) {
-    try{
+    try {
         let keys = isProd ? JSON.parse(await loadSecrets()) : null;
         ctx.session.env['triton'] = keys ? keys.tritonToken : process.env.TRITON_RPC_TOKEN;
-    }catch(error:any){
-        logErrorToFile("_loadEnvVars",error);
+    } catch (error: any) {
+        logErrorToFile("_loadEnvVars", error);
         console.error(error);
     }
 }
 
-export function loadSecrets():any {
-    const secret_name = "mvx-bot-db"
-    const client = new SecretsManagerClient({region: "ca-central-1"});
-    let response;
-    try {
-         client.send(new GetSecretValueCommand({SecretId: secret_name})).then((data) => {
-            if(data){response = data.SecretString;}
-         });
-    } catch (error: any) {
-        throw error;
-    }
-    return response && response;
+export function loadSecrets(): any {
+    return new Promise((resolve, reject) => {
+        const secret_name = "mvx-bot-db"
+        const client = new SecretsManagerClient({ region: "ca-central-1" });
+        try {
+            client.send(new GetSecretValueCommand({ SecretId: secret_name })).then((data) => {
+                if (data) { resolve(data.SecretString) }
+            });
+        } catch (error: any) {
+            reject();
+        }
+    })
+
+
 }
