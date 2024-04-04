2f7ae2a0994ad206410f633c7c4387c60aaec000
ghaleb-d <gh.damaj@gmail.com>
2024-04-04 04:26:22 -0400

    test

--- src/service/feeds/index.ts
+++ src/service/feeds/index.ts
@@ -9,9 +9,6 @@ import {
 import { Metaplex } from "@metaplex-foundation/js";
 
 export async function getTokenMetadata(ctx: any, tokenAddress: string): Promise<any> {
-    // console.log('tokenAddress',tokenAddress)
-    // console.log('ctx.session',ctx.session)
-    // const tokenAddress = ctx.session.portfolio.activeTradingPool.baseMint;
     const chatId = ctx.chat.id;
     if (!isValidBase58(tokenAddress)) {
         console.error('Invalid token address:', tokenAddress);
@@ -22,45 +19,15 @@ export async function getTokenMetadata(ctx: any, tokenAddress: string): Promise<
     const metaplex = Metaplex.make(connection);
     const mintAddress = new PublicKey(tokenAddress);
     const tokenData = await metaplex.nfts().findByMint({ mintAddress: mintAddress });
-
-    const activeWalletIndexIdx: number = ctx.session.activeWalletIndex;
-    const publicKeyString = ctx.session.portfolio.wallets[activeWalletIndexIdx]!.publicKey;
-    // const solanaDetails = await getSolanaDetails();
-   
-    // const liquidityInfo = await getLiquidityFromDextools(tokenAddress);
-    // const marketCap = await getMarketCapFromDextools('solana', tokenAddress);
-    // const holdersToken = await getHoldersFromDextools('solana', tokenAddress);
-    // const priceChanges = await getPriceChangesFromDextools(tokenAddress);
-    // Call the refactored function with the results of the API calls
-
-    // const decimals = token.mint.decimals;
-    // const userTokenDetails = await getUserTokenBalanceAndDetails(new PublicKey(publicKeyString), new PublicKey(tokenAddress),connection);
     const birdeyeURL = `https://birdeye.so/token/${tokenAddress}?chain=solana`;
     const dextoolsURL = `https://www.dextools.io/app/solana/pair-explorer/${tokenAddress}`;
     const dexscreenerURL = `https://dexscreener.com/solana/${tokenAddress}`;
-    // const formattedLiquidity = await formatNumberToKOrM(Number(tokenInfo.liquidity)) ?? "N/A";
-    // const formattedmac = await formatNumberToKOrM(tokenInfo.mc) ?? "NA";
-    // formattedpooledSol =  liquidityInfo.sideTokenReserve
-
-    // Process the data received from the API calls
-    // const solPriceInUSD = solanaDetails.toFixed(3);
-    // const balanceInSOL = await getSolBalance(publicKeyString,connection);
-    // const balanceInUSD = (balanceInSOL * (solanaDetails).toFixed(2));
     return {
-        // solanaDetails,
         tokenData,
-        // tokenInfo,
-        // tokenCreator,
-        // tokenCreatorPercentage,
-        // userTokenDetails,
         birdeyeURL,
         dextoolsURL,
         dexscreenerURL,
-        // formattedLiquidity,
-        // formattedmac,
-        // solPriceInUSD,
-        // balanceInSOL,
-        // balanceInUSD,
+
     }
 }
 
