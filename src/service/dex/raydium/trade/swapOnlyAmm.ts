bba0278d49bfcc1be5ffbbf2401647e85c92bc08
Daniel G <danielifguerrero@gmail.com>
2024-04-04 21:36:47 -0500

    add units *2

--- src/service/dex/raydium/trade/swapOnlyAmm.ts
+++ src/service/dex/raydium/trade/swapOnlyAmm.ts
@@ -184,7 +184,7 @@ export async function swapOnlyAmm(input: TxInputInfo) {
 
   if (units) {
     console.log("units: ",units);
-    units = Math.ceil(units); // margin of error
+    units = Math.ceil(units*2); // margin of error
     innerTransactions[0].instructions.push(ComputeBudgetProgram.setComputeUnitLimit({ units: units }));
   }
 
